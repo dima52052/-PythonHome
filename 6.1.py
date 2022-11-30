884482c128726c1a564c9a3d6091b59f2ec3cabc
Ershov Dmitrii <dima52052@mail.ru>
2022-11-30 14:03:13 +0500

    add 6.1.py and 6.2.py

--- 6.1.py
+++ 6.1.py
@@ -3,12 +3,37 @@
 # 2+2 => 4;
 # 1+2*3 => 7;
 # 1-2*3 => -5;
+# Добавьте возможность использования скобок, меняющих приоритет операций.
+# Пример:
+# 1+2*3 => 7;
+# (1+2)*3 => 9;
 
-
-day = int(input('Введите день недели 1-7: '))
-if day > 7 or day < 1:
-     print('Введите с 1-7')
-elif day == 6 or day == 7:
-    print("Да, это выходной")
-else:
-     print("Нет, это не выходной")
+import re
+ 
+ 
+actions = {
+  "^": lambda x, y: str(float(x)**float(y)),
+  "*": lambda x, y: str(float(x) * float(y)),
+  "/": lambda x, y: str(float(x) / float(y)),
+  "+": lambda x, y: str(float(x) + float(y)),
+  "-": lambda x, y: str(float(x) - float(y))
+}
+ 
+priority_reg_exp = r"\((.+?)\)"
+action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+ 
+ 
+ 
+def my_eval(expresion: str) -> str:
+ 
+    while (match := re.search(priority_reg_exp, expresion)):
+        expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+ 
+    for symbol, action in actions.items():
+        while (match := re.search(action_reg_exp.format(symbol), expresion)):
+            expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+ 
+    return expresion
+ 
+exp = input('Введите пример: ')
+print(my_eval(exp))  
